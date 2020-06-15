369c94e585e8b2c0774a3a1d3687de1ced309ac1
Prasanta Kumar Sahoo <sahoop092@gmail.com>
2020-06-15 17:18:34 +0530

    Java Assignement
    Java Assignement - 3

---
+++ FinishingSchoolProject/com.fs.streamapi/com/fs/steams/ArraySteams.java
@@ -0,0 +1,18 @@
+package com.fs.steams;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArraySteams {
+
+	public static void main(String[] args) {
+
+		int a[] = { 22, 33, 44, 55, 66, 77, 88, 99 };
+
+		System.out.println("Sum is " + IntStream.of(a).sum());
+
+		IntStream stream = Arrays.stream(a);
+		stream.forEach(str -> System.out.print(str + " "));
+
+	}
+}
